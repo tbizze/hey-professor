e7825852f32b98b80e8e450af63d0fc9234bc9da
Tiago Bizze <Tiago Bizze>
2024-08-14 22:41:32 -0300

    HEY-9: criando seeder e factory para model Question

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\{Question, User};
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name'  => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        Question::factory()->count(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
